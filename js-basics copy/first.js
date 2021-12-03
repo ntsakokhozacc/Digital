818a80c8aa0af8f1c04a8fa2f324b5cc787924dd
Academy_learner <academy_learner@Academy-learners-MacBook-Air.local>
2021-12-03 12:35:38 +0200

    Changes made to first.js

--- js-basics copy/first.js
+++ js-basics copy/first.js
@@ -1,6 +1,4 @@
 console.log('This is a test');
-<<<<<<< HEAD
 console.log('Lets get this code done');
-=======
 console.log("Vee is just everywhere");
->>>>>>> fe51ee1c105281e476c638b64b5dab282dffb9c0
+console.log("Vero is just everywhere");
